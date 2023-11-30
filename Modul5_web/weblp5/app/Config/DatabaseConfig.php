a0d9d81ad5e3899be1a571b2d0dc2c548f4f891c
marwasaniyya <127455920+marwasaniyya@users.noreply.github.com>
2023-11-30 08:22:47 +0700

    Add files via upload

---
+++ Modul5_web/weblp5/app/Config/DatabaseConfig.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace app\Config;
+
+class DatabaseConfig
+{
+
+    // SETTING KONFIGURASI DATABASE KALIAN
+    public $host = "localhost";
+    public $user = "root";
+    public $password = "";
+    public $database_name = "praktikum";
+    public $port = 3306;
+}
